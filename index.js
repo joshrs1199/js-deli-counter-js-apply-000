54245516a835303b3184e195ccb8c67de4dce120
Joshua Rivera-Sampayo <josh.rs1199@gmail.com>
2019-01-24 14:30:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ function nowServing(line){
   }
   return "Currently serving " + line.shift() + ".";
 }
+function currentLine(line){
+  if (line.length === 0){
+    return "The line is currently empty."
+  }
+  var i;
+  var z = "The line is currently: ";
+  for (i = 0; i < line.length; i++){
+    if (i === line.length - 1){
+      z += i + 1 + ". " + line[i];
+    }
+    else z += i + 1 + ". " + line[i] + ", ";
+  }
+  return z 
+}

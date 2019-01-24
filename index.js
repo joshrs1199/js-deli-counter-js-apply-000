c9c2d4ca39b1da4937b12d77adcd3795d44e9dac
Joshua Rivera-Sampayo <josh.rs1199@gmail.com>
2019-01-24 14:18:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ function takeANumber(line, name){
   line.push(name);
   return "Welcome, " + name + ". You are number " + line.length + " in line.";
 }
+function nowServing(line){
+  if (line.length === 0){
+    return "There is nobody waiting to be served!";
+  }
+  return line.shift();
+}
